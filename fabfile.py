422610664fe8d4f98d5bcedd959fbe877d7b5ea3
Alexander Pugachev <apugachev@griddynamics.com>
2012-04-03 17:42:40 +0300

    cleaner fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,9 @@
 from fabric.api import *
 
-env.hosts = ['root@127.0.0.1:2222'] # this host has nova client and creds
+try:
+    from local_fab_settings import *
+except ImportError:
+    pass
 
 def test():
     local('python tests.py')
@@ -10,24 +13,17 @@ def dev():
 
 def generate_dev_settings():
     local('''echo "DEBUG = True
+
 DB_HOST = 'localhost'
 DB_USER = 'nova'
 DB_PASS = 'nova'
 DB_NAME = 'keystone'" > local_settings.py''')
 
-def test_remotely():
-    # spawn a node
-    spawned = run('nova boot --flavor 1 --image 9 --key_name my-gd-key toolza-test-remote-1', )
-    
-    # install mysql
-    run()
-    # feed a dump
-    # install openldap
-    # create record
-    # spawn a node
-    # create bare git repo
-    # push changes
-    # reset repo
-    # prepare test settings
-    # run tests
-    run('echo "hello"')
+def up():
+    local('git push origin master')
+
+def stage():
+    local(
+        'sudo chroot %s ssh %s "cd C4GD-web; git pull; pip-python install -r requirements.pip"' % (
+            NOVA_CHROOT_PATH,
+            STAGING_IP))
