3bccba12e9f1011d7643213f832b7d31b02f4429
Alexander Pugachev <apugachev@griddynamics.com>
2012-05-08 12:54:27 +0300

    roleless users still create problems

--- C4GD_web/context_processors.py
+++ C4GD_web/context_processors.py
@@ -17,14 +17,13 @@ def frequent_data():
     if hasattr(g, 'user'):
         tenants_with_roles = []
         ids = list(g.user.user_roles.values(UserRole.tenant_id))
-        tenants = g.store.find(Tenant, Or(*[Tenant.id == x for x in ids])).order_by(Tenant.name)
-        for tenant in tenants:
-            tenants_with_roles.append(
-                (
-                    tenant,
-                    [x.role for x in tenant.user_roles.find(user_id=g.user.id).order_by(UserRole.role_id)]
-                    ))
-        return {
-            'tenants_with_roles': tenants_with_roles
-            }
+        if len(ids):
+            tenants = g.store.find(Tenant, Or(*[Tenant.id == x for x in ids])).order_by(Tenant.name)
+            for tenant in tenants:
+                tenants_with_roles.append(
+                    (
+                        tenant,
+                        [x.role for x in tenant.user_roles.find(user_id=g.user.id).order_by(UserRole.role_id)]
+                        ))
+        return {'tenants_with_roles': tenants_with_roles}
     return {}
