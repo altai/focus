775ef1d23620ebbcd7b37031cbc9c687642a74b7
Stanislav Pugachev <spugachev@griddynamics.com>
2012-05-30 10:24:57 +0300

    default port setting for app runserver

--- C4GD_web/runserver.py
+++ C4GD_web/runserver.py
@@ -5,5 +5,5 @@ from C4GD_web import app
 
 if __name__ == "__main__":
     from C4GD_web import app
-    app.run(host='0.0.0.0')
-
+    port = app.config['DEFAULT_APP_PORT']
+    app.run(host='0.0.0.0', port=port if port else 5000)
