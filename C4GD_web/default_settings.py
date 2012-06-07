063bb64dede8350f6b32e2699729f7192e35f094
Alexander Pugachev <apugachev@griddynamics.com>
2012-06-07 10:31:58 +0300

    -ldap

--- C4GD_web/default_settings.py
+++ C4GD_web/default_settings.py
@@ -4,8 +4,6 @@ SECRET_KEY = 'g.U(\x8cQ\xbc\xdb\\\xc3\x9a\xb2\xb6,\xec\xad(\xf8"2*\xef\x0bd'
 NEXT_TO_LOGIN_ARG = 'next' # GET/POST field name to store next after login URL
 DEFAULT_NEXT_TO_LOGIN_VIEW = 'dashboard' # no next? redirect to this view
 DEFAULT_NEXT_TO_LOGOUT_VIEW = 'dashboard'
-LDAP_URI = 'ldap://ns/' 
-LDAP_BASEDN = 'ou=people,ou=griddynamics,dc=griddynamics,dc=net'
 
 BILLING_URL = 'http://172.30.0.3:8787/v2'
 TEMPLATE_EXTENSION = '.haml'
