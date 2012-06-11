5a9b6e26ff0789b7e85792caacc02b0fc14c6428
Alexander Pugachev <apugachev@griddynamics.com>
2012-06-11 13:58:06 +0300

    few comments added to images bp

--- C4GD_web/views/images.py
+++ C4GD_web/views/images.py
@@ -19,7 +19,7 @@ from C4GD_web.views import pagination
 
 
 def get_bp(name):
-    '''Return blueprint for Image management.
+    """Return blueprint for Image management.
 
     index (list images), create image(upload), delete image.
     Each view function of the blueprint gets protected by Principal.
@@ -29,15 +29,23 @@ def get_bp(name):
     
     Now this works for global admins only. Later we'll find out how to 
     implelement this for project memebers as well.
-    '''
+    """
     bp = blueprints.Blueprint(name, __name__)
     
     @bp.url_value_preprocessor
     def global_or_project(endpoint, values):
+        """Binds project id to g if project od exists.
+
+        The blueprint can be used in a project or in admin context.
+        """
         flask.g.project_id = values.pop('project_id', None)
 
     @bp.before_request
     def authorize():
+        """Check user permissions.
+
+        Depends on project id existing.
+        """
         if flask.g.project_id:
             need = ('role', 'member', flask.g.project_id)
         else:
@@ -46,8 +54,10 @@ def get_bp(name):
 
     @bp.route('<image_id>/', methods=['GET'])
     def show(image_id):
-        """
-        Present details page for single image object
+        """Present details page for single image object.
+
+        Tries using both Glance and Nova services because they provide
+        different info set about an image.
         """
         glance_image = clients.glance.images.get(image_id)
         nova_image = clients.nova.images.get(image_id)
@@ -56,10 +66,11 @@ def get_bp(name):
 
     @bp.route('')
     def index():
-        '''
-        Present images of global or project visibility level
+        """List images.
         
-        '''
+        Admin (global visibility level) should see only images from 
+        systenant. Project members should see images for projects only.
+        """
         images = abstract.Image.list()
         p = pagination.Pagination(images)
         data = p.slice(images)
@@ -71,15 +82,19 @@ def get_bp(name):
 
     @bp.route('new/', methods=['GET', 'POST'])
     def new():
-        '''
-        Present add image form, save add image form.
+        """Upload image.
+
+        New image uploaded in tenant systenant publicly if blueprint is in
+        admin context. Otherwise image uploaded in the tenant used for the
+        project and is not public. With default Glance settigns this would
+        restrict access to the image for members of the project.
 
         POST:
         - validate
         - create image
         - cleanup tmp file
         - return successful message
-        '''
+        """
         if flask.request.method == 'POST':
             # TODO(apugachev): validate thoroughly, do not rely on js to do it
             
@@ -111,6 +126,7 @@ def get_bp(name):
                 try:
                     os.unlink(path)
                 except OSError:
+                    # nothing to do, temporal file was removed by something
                     pass
 
         images = abstract.Image.list(limit=1000000)
@@ -126,11 +142,11 @@ def get_bp(name):
 
     @bp.route('new/upload/', methods=['POST'])
     def upload():
-        '''
+        """
         Saves uploaded filed.
 
         save, return filename.
-        '''
+        """
         storage = flask.request.files['file']
         filename = C4GD_web.files_uploads.save(storage)
         return filename
@@ -150,4 +166,3 @@ def get_bp(name):
         return flask.redirect(flask.url_for('.index'))
             
     return bp
-
