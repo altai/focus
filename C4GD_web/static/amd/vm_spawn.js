4ee0d938f5e233ae2db21f7562dedc0ea887cff7
Alexander Pugachev <apugachev@griddynamics.com>
2012-06-14 19:14:19 +0300

    commented problematic code from dkhovyakov

--- C4GD_web/static/amd/vm_spawn.js
+++ C4GD_web/static/amd/vm_spawn.js
@@ -1,29 +1,29 @@
-function print_image(data){
-}
+// function print_image(data){
+// }
 
-function print_flavor(data){
-}
+// function print_flavor(data){
+// }
 
-function get_image(){
-  val = $("#image option:selected").first().val()
-  alert("/images/" + val)
-  $.get("/images/" + val, "", function(data) {
-    alert(data)
-  })
-}
+// function get_image(){
+//   val = $("#image option:selected").first().val()
+//   alert("/images/" + val)
+//   $.get("/images/" + val, "", function(data) {
+//     alert(data)
+//   })
+// }
 
-function get_flavor(data){
-  val = $("#flavor option:selected").first().val()
-  alert("/flavors/detail/" + val)
-  $.get("/flavors/detail/" + val, "", function(data) {
-    alert(data)
-  })
-}
+// function get_flavor(data){
+//   val = $("#flavor option:selected").first().val()
+//   alert("/flavors/detail/" + val)
+//   $.get("/flavors/detail/" + val, "", function(data) {
+//     alert(data)
+//   })
+// }
 
-curl(curl_cfg, ['jq'], function($){
-  $(document).ready(function(){
-    $("#image").change(get_image)
-    $("#flavor").change(get_flavor)
-  });
-});
+// curl(curl_cfg, ['jq'], function($){
+//   $(document).ready(function(){
+//     $("#image").change(get_image)
+//     $("#flavor").change(get_flavor)
+//   });
+// });
 
