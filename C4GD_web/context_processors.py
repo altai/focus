2f8607f69f8a486cf3d9dcb30f26b0dbd674adcc
Alexander Pugachev <apugachev@griddynamics.com>
2012-06-15 10:59:06 +0300

    projects list updated immediately after user's membership is changed

--- C4GD_web/context_processors.py
+++ C4GD_web/context_processors.py
@@ -5,17 +5,34 @@ import urlparse
 
 from flask import g, session, request
 from C4GD_web import app
-
+from C4GD_web import clients
+from C4GD_web import utils
 
 @app.context_processor
 def navigation_bar_tenant_data():
     try:
         if getattr(g, 'is_authenticated', False):
             tenants_with_roles = []
-            for tenant in session['tenants']:
-                tenants_with_roles.append(
-                    (tenant, session['keystone_scoped'][tenant['id']]['access']['user']['roles']))
+            # keystone user id
+            user_id = session['keystone_unscoped']['access']['user']['id']
+            # TODO(apugachev) use simpler way when keystone works correctly
+            # now use simple iteration through all tenants and users
+            #  user = clients.clients.keystone.users.get(user_id)
+            #  roles = user.list_roles()
+            tenants = clients.clients.keystone.tenants.list()
+            for tenant in tenants:
+                users = tenant.list_users()
+                if filter(lambda x: x.id == user_id, users):
+                    # TODO(apugachev) take roles from keystone API 
+                    # when list_roles() works instead of obtain_scoped
+                    if tenant.id not in session['keystone_scoped']:
+                        utils.obtain_scoped(tenant.id)
+                    # NOTE(apugachev) templates expect dicts
+                    tenants_with_roles.append(
+                        (tenant._info, session['keystone_scoped'][tenant.id]\
+                             ['access']['user']['roles']))
             return {'tenants_with_roles': tenants_with_roles}
+
     except Exception:
         exc_type, exc_value, tb = sys.exc_info()
         app.log_exception((exc_type, exc_value, tb))
@@ -32,8 +49,11 @@ def url_for_other_page(page):
                 tuple(args.iterlists()), 
                 doseq=1))
         return result
+
     except Exception:
         exc_type, exc_value, tb = sys.exc_info()
         app.log_exception((exc_type, exc_value, tb))
     return {}
+
+
 app.jinja_env.globals['url_for_other_page'] = url_for_other_page
