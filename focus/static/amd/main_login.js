f3a769d9246700414b26a8a4db0cb79e1e867738
Alexander Pugachev <apugachev@griddynamics.com>
2012-07-09 16:18:32 +0300

    js tweak

--- focus/static/amd/main_login.js
+++ focus/static/amd/main_login.js
@@ -1,5 +1,10 @@
 curl(curl_cfg, ['jq'], function($){
   $(document).ready(function(){
-    $('form.login input#username').focus();
+    $email = $('form.login input#email');
+    $email.focus();
+    $('form.login a').click(function(e){
+      $a = $(this);
+      $a.attr('href', $a.attr('href') + '?email=' + $email.val());
+    });
   });
 });
