17bccc0145e036c7ce49915c8a204635116deac1
Alexander Pugachev <apugachev@griddynamics.com>
2012-07-13 14:38:13 +0300

    limiting number of workers to pragmatic limits

--- etc/gunicorn_config.py
+++ etc/gunicorn_config.py
@@ -1,7 +1,15 @@
 import multiprocessing
 
 bind = "0.0.0.0:8080"
-workers = multiprocessing.cpu_count() * 2 + 1
+
+formula_workers_count = multiprocessing.cpu_count() * 2 + 1
+if formula_workers_count < 4:
+    workers = 4
+elif formula_workers_count > 12:
+    workers = 12
+else:
+    workers = formula_workers_count
+
 timeout = 86400
 accesslog = '/var/log/focus/gunicorn.access-log'
 errorlog = '/var/log/focus/gunicorn.error-log'
