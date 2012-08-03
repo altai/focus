fc8a64852e49cb13a5989610edcd383b2c63cba6
Stanislav Pugachev <spugachev@griddynamics.com>
2012-08-03 05:36:50 -0400

    upload ui improovments

--- focus/static/amd/jq.charged.js
+++ focus/static/amd/jq.charged.js
@@ -1,7 +1,6 @@
 define(
 [
   'js!amd/jq.vanilla.js!order', 
-  'js!vendors/twitter-bootstrap-aaabe2a/js/bootstrap-alert.js!order',
   'js!vendors/twitter-bootstrap-aaabe2a/js/bootstrap-transition.js!order',
   'js!vendors/twitter-bootstrap-aaabe2a/js/bootstrap-collapse.js!order', 
   'js!vendors/twitter-bootstrap-aaabe2a/js/bootstrap-dropdown.js!order',
