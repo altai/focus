fea2b9f7bab7ec39f85211b81a873c05a8e93f8d
Alexander Pugachev <apugachev@griddynamics.com>
2012-09-13 19:27:46 +0300

    increasing number of workers to avoid deficit of zabbix proxy service workers

--- etc/gunicorn_config.py
+++ etc/gunicorn_config.py
@@ -3,12 +3,6 @@ import multiprocessing
 bind = "0.0.0.0:8080"
 
 formula_workers_count = multiprocessing.cpu_count() * 2 + 1
-if formula_workers_count < 4:
-    workers = 4
-elif formula_workers_count > 12:
-    workers = 12
-else:
-    workers = formula_workers_count
 
 timeout = 86400
 accesslog = '/var/log/focus/gunicorn.access-log'
